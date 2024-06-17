571805934e7bd36a181fe45f0da290f1a06f8883
Tom <tomburdge3@gmail.com>
2024-06-17 23:12:24 +0100

    feature: add environment setup files

---
+++ harley/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import polars as pl
+
+
+from harley.utils import parse_into_expr, register_plugin, parse_version
+from harley.dataframe_helper import column_to_list
+
+if TYPE_CHECKING:
+    from polars.type_aliases import IntoExpr
+
+if parse_version(pl.__version__) < parse_version("0.20.16"):
+    from polars.utils.udfs import _get_shared_lib_location
+
+    lib: str | Path = _get_shared_lib_location(__file__)
+else:
+    lib = Path(__file__).parent
+
+__all__ = ["column_to_list"]
---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+mod expressions;
+mod utils;
+
+
+#[cfg(target_os = "linux")]
+use jemallocator::Jemalloc;
+
+#[global_allocator]
+#[cfg(target_os = "linux")]
+static ALLOC: Jemalloc = Jemalloc;
+
+use pyo3::types::PyModule;
+use pyo3::{pymodule, PyResult, Bound};
+use pyo3::Python;
+
+#[pymodule]
+fn harley(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    Ok(())
+}
