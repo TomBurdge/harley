f983f02a51867d7a8f328c6accffd6941d0750bc
Lyndon Fan <lyndon0808@gmail.com>
2024-06-19 00:50:42 +0100

    feat: impl & test single_space

--- src/expressions.rs
+++ src/expressions.rs
@@ -13,3 +13,28 @@ use std::fmt::Write;
 //     });
 //     Ok(out.into_series())
 // }
+
+#[polars_expr(output_type=String)]
+fn single_space(inputs: &[Series]) -> PolarsResult<Series> {
+    let ca: &StringChunked = inputs[0].str()?;
+    let out: StringChunked = ca.apply_to_buffer(|value: &str, output: &mut String| {
+        if value == "" {return;}
+        let mut start_with_white_space = value.chars().next().unwrap().is_whitespace();
+        let mut to_add_space = false;
+        for c in value.chars() {
+            if c.is_whitespace() {
+                to_add_space = true;
+            } else {
+                if to_add_space {
+                    if !start_with_white_space {
+                        write!(output, " ").unwrap();
+                    }
+                    start_with_white_space = false;
+                    to_add_space = false;
+                }
+                write!(output, "{}", c).unwrap();
+            }
+        }
+    });
+    Ok(out.into_series())
+}
---
+++ tests/test_string_functions.py
@@ -0,0 +1,25 @@
+import pytest
+
+import polars as pl
+from polars.testing import assert_series_equal
+
+import harley
+
+@pytest.mark.parametrize("lazy", [True, False])
+def test_single_space(lazy: bool):
+    string_data = [
+        ("  I  go   ", "I go"),
+        ("    to", "to"),
+        ("school   ", "school"),
+        ("by bus", "by bus"),
+        (None, None),
+    ]
+    frame = pl.DataFrame([{"input": inp, "expected": exp} for inp, exp in string_data])
+    if lazy:
+        frame = frame.lazy()
+    frame = frame.with_columns(
+        actual = harley.single_space("input")
+    )
+    if lazy:
+        frame = frame.collect()
+    assert_series_equal(frame["actual"], frame["expected"], check_names=False)
