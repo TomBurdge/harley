09ff32e5aa2e6099c9a2c219f84a2652ba427929
Tom <tomburdge3@gmail.com>
2024-06-25 17:36:12 +0100

    format: remove comments

--- src/expressions.rs
+++ src/expressions.rs
@@ -8,24 +8,24 @@ use pyo3::exceptions::PyValueError;
 #[pyfunction]
 pub fn two_columns_to_dictionary(py: Python, pydf: PyDataFrame, col1: &str, col2: &str) -> PyResult<Py<PyDict>> {
     let df: DataFrame = pydf.into();
-    let series1 = df.column(col1).map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("{}", e)))?;
-    let series2 = df.column(col2).map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("{}", e)))?;
+    let series1 = df.column(col1).map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("Error on selecting col1 {}", e)))?;
+    let series2 = df.column(col2).map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("Error on selecting col2 {}", e)))?;
 
     fn convert_series_value(py: Python, value: AnyValue) -> PyResult<PyObject> {
         match value {
             AnyValue::String(val) => Ok(PyString::new_bound(py, val).into_py(py)),
             AnyValue::Int64(val) => Ok(val.into_py(py)),
-            AnyValue::UInt64(val) => Ok((val as i64).into_py(py)), // Python's int can handle large integers
+            AnyValue::UInt64(val) => Ok((val as i64).into_py(py)),
             AnyValue::Float64(val) => Ok(PyFloat::new_bound(py, val).into_py(py)),
-            AnyValue::Null => Ok(py.None()), // Handle null values
+            AnyValue::Null => Ok(py.None()),
             AnyValue::Boolean(val) => Ok(val.into_py(py)),
-            AnyValue::Float32(val) => Ok(PyFloat::new_bound(py, val as f64).into_py(py)), // Convert Float32 to Float64
+            AnyValue::Float32(val) => Ok(PyFloat::new_bound(py, val as f64).into_py(py)),
             AnyValue::Int32(val) => Ok(val.into_py(py)),
-            AnyValue::UInt32(val) => Ok((val as i64).into_py(py)), // Python's int can handle large integers
+            AnyValue::UInt32(val) => Ok((val as i64).into_py(py)),
             AnyValue::Int16(val) => Ok(val.into_py(py)),
-            AnyValue::UInt16(val) => Ok((val as i64).into_py(py)), // Python's int can handle large integers
+            AnyValue::UInt16(val) => Ok((val as i64).into_py(py)),
             AnyValue::Int8(val) => Ok(val.into_py(py)),
-            AnyValue::UInt8(val) => Ok((val as i64).into_py(py)), // Python's int can handle large integers
+            AnyValue::UInt8(val) => Ok((val as i64).into_py(py)),
             AnyValue::Date(val) => Ok(val.into_py(py)),
             AnyValue::Datetime(val, _, _) => Ok(val.into_py(py)),
             AnyValue::Duration(val, _) => Ok(val.into_py(py)),
