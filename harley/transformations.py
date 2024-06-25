7f5d3e5d7a0cb8bacc27f4957660804351872caa
Tom <tomburdge3@gmail.com>
2024-06-25 23:05:03 +0100

    format: remove whitespace

--- harley/transformations.py
+++ harley/transformations.py
@@ -1,7 +1,6 @@
 from harley.utils import PolarsFrame
 from .harley import columns_to_snake_case
 
-
 def snake_case_column_names(df: PolarsFrame) -> PolarsFrame:
     new_col_names = columns_to_snake_case(df.columns)
     return df.rename(new_col_names)
