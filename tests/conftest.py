03009c6406fe49666084cbb65c4856db5990642c
Tom <tomburdge3@gmail.com>
2024-06-28 18:11:27 +0100

    feature: add lazyframe test to is_null_or_blank

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,11 @@
-from polars import DataFrame, LazyFrame
-polars_frames =[DataFrame, LazyFrame]
+from polars import DataFrame, LazyFrame, DataType
+from typing import List, Any
+
+polars_frames = [DataFrame, LazyFrame]
+
+def combine_pytest_params_with_dtypes(dtypes: List[DataType], parameters: List[List[Any]]) -> List[List[Any]]:
+    dtype_column_combinations = []
+    for params in parameters:
+        for dtype in dtypes:
+            dtype_column_combinations.append(tuple([dtype] + params))
+    return dtype_column_combinations
--- tests/test_booleans.py
+++ tests/test_booleans.py
@@ -1,17 +1,20 @@
 import pytest
-from tests.conftest import polars_frames
+from tests.conftest import polars_frames,combine_pytest_params_with_dtypes
 import polars as pl
 from harley.to_boolean import is_null_or_blank
 from polars import DataFrame, DataType, LazyFrame
 from polars.testing import assert_frame_equal
 from typing import List
 
-@pytest.mark.parametrize("frame_type, inp, exp, all_white_space_as_null",     (
-    (pl.DataFrame, [" ", "", "not blank", None], [False, True, False, True],False),
-    (pl.DataFrame, [" ", "", "not blank", None],[True, True, False, True],True),
-    (pl.DataFrame, [[], [123], None],[True, False, True], False)
-    ))
+params = combine_pytest_params_with_dtypes(polars_frames, [
+    [[" ", "", "not blank", None], [False, True, False, True],False],
+    [[" ", "", "not blank", None],[True, True, False, True],True],
+    [[[], [123], None],[True, False, True], False]
+    ])
+
+@pytest.mark.parametrize("frame_type, inp, exp, all_white_space_as_null",     params)
 def test_is_null_or_blank(frame_type:DataType, inp:List, exp:List,all_white_space_as_null:bool):
+    
     data = frame_type({"value":inp})
     if isinstance(res := data.select(res = is_null_or_blank("value", all_white_space_as_null)), LazyFrame):
         res = res.collect()
