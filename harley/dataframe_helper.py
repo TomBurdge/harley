4539988f3862469bc4f197ea08a3d157617b1fa7
Tom <tomburdge3@gmail.com>
2024-06-30 16:52:14 +0100

    feature: two columns to dictionary

--- harley/dataframe_helper.py
+++ harley/dataframe_helper.py
@@ -1,6 +1,7 @@
 from harley.utils import PolarsFrame
-from polars import LazyFrame
-from typing import List
+from polars import LazyFrame, Array, Struct, DataFrame
+from polars import List as PolarsList
+from typing import List, Dict, Any
 
 
 def column_to_list(df: PolarsFrame, column: str) -> List:
@@ -8,3 +9,16 @@ def column_to_list(df: PolarsFrame, column: str) -> List:
         df = df.collect()
     series = df.select(column).to_series()
     return series.to_list()
+
+
+def two_columns_to_dictionary(
+    df: DataFrame, key_col_name: str, value_col_name: str
+) -> Dict[Any, Any]:
+    df = df.select(key_col_name, value_col_name)
+    if df.dtypes[0] in [Array, PolarsList, Struct]:
+        raise ValueError(
+            f"Column {key_col_name} is of type {df.dtypes[0]} will not return a hashable type.",
+            f"Therefore {key_col_name} cannot provide an acceptable key",
+        )
+    df = df.to_dicts()
+    return {row[key_col_name]: row[value_col_name] for row in df}
--- tests/test_dataframe_helper.py
+++ tests/test_dataframe_helper.py
@@ -2,6 +2,8 @@ from harley import column_to_list
 import pytest
 from datetime import datetime
 from tests.conftest import polars_frames
+from harley.dataframe_helper import two_columns_to_dictionary
+from polars import DataFrame
 
 floats=[4.0, 5.0, 6.0, 7.0, 8.0]
 data = {
@@ -23,3 +25,23 @@ def test_column_to_list(frame_type:str):
     exp = floats
     res = column_to_list(frame, "float")
     assert exp==res
+
+
+def test_two_columns_to_dictionary_passes():
+    inp_col_key_vals = [i for i in range(9)]
+    inp_col_key="inp_col_key"
+    inp_col_val_vals = [i for i in range(1, 10)]
+    inp_col_val="inp_col_val"
+    df = DataFrame({inp_col_key:inp_col_key_vals, inp_col_val:inp_col_val_vals})
+    exp = dict(zip(inp_col_key_vals, inp_col_val_vals))
+    res = two_columns_to_dictionary(df = df, key_col_name=inp_col_key, value_col_name=inp_col_val)
+    assert res==exp
+
+def test_two_columns_to_dictionary_raises_error_on_nested_keys():
+    inp_col_key_vals = [[123],[234]]
+    inp_col_key="inp_col_key"
+    inp_col_val_vals = [i for i in range( 2)]
+    inp_col_val="inp_col_val"
+    df = DataFrame({inp_col_key:inp_col_key_vals, inp_col_val:inp_col_val_vals})
+    with pytest.raises(ValueError):
+        two_columns_to_dictionary(df = df, key_col_name=inp_col_key, value_col_name=inp_col_val)
