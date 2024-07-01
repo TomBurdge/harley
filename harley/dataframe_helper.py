3806d185df3a1aa8a9caef9a35ad749343d5c2ff
Tom <tomburdge3@gmail.com>
2024-07-01 20:44:46 +0100

    feature: check for duplicate keys

--- harley/dataframe_helper.py
+++ harley/dataframe_helper.py
@@ -1,6 +1,6 @@
 from harley.utils import PolarsFrame
 from polars import LazyFrame, Object, Unknown, DataFrame
-from polars import List as PolarsList
+from polars import col
 from typing import List, Dict, Any
 
 
@@ -12,10 +12,13 @@ def column_to_list(df: PolarsFrame, column: str) -> List:
 
 
 def two_columns_to_dictionary(
-    df: DataFrame, key_col_name: str, value_col_name: str
+    df: DataFrame,
+    key_col_name: str,
+    value_col_name: str,
+    allow_duplicates_keys: bool = False,
 ) -> Dict[Any, Any]:
     df = df.select(key_col_name, value_col_name)
-    key_dtype =df.dtypes[0]
+    key_dtype = df.dtypes[0]
     if key_dtype.is_nested():
         raise ValueError(
             f"Column {key_col_name} is of type {df.dtypes[0]} will not return a hashable type.",
@@ -26,5 +29,16 @@ def two_columns_to_dictionary(
             f"Column {key_col_name} is of type {df.dtypes[0]} and may not return a hashable type.",
             f"Therefore {key_col_name} cannot provide an acceptable key.",
         )
+    if not allow_duplicates_keys:
+        if df.filter(col(key_col_name).is_duplicated()).height > 0:
+            raise ValueError(
+                "Duplicate records found in key column.",
+                "Duplicate keys will be overwritten and de-duplicated unpredicatbly.",
+                "To allow duplicate keys, set `allow_duplicate_keys` to True",
+                "Some of the duplicates:",
+                column_to_list(
+                    df.filter((col(key_col_name).is_duplicated())), key_col_name
+                )[:3],
+            )
     df = df.to_dicts()
     return {row[key_col_name]: row[value_col_name] for row in df}
--- tests/test_dataframe_helper.py
+++ tests/test_dataframe_helper.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from tests.conftest import polars_frames
 from harley.dataframe_helper import two_columns_to_dictionary
 from polars import DataFrame
+from polars.testing import assert_frame_equal
 
 floats=[4.0, 5.0, 6.0, 7.0, 8.0]
 data = {
@@ -45,3 +46,20 @@ def test_two_columns_to_dictionary_raises_error_on_nested_keys():
     df = DataFrame({inp_col_key:inp_col_key_vals, inp_col_val:inp_col_val_vals})
     with pytest.raises(ValueError):
         two_columns_to_dictionary(df = df, key_col_name=inp_col_key, value_col_name=inp_col_val)
+
+@pytest.fixture()
+def duplicate_keys_df() -> DataFrame:
+    inp_col_key_vals = ["duplicate","duplicate", "not duplicate"]
+    inp_col_key="inp_col_key"
+    inp_col_val_vals = [i for i in range(3)]
+    inp_col_val="inp_col_val"
+    return DataFrame({inp_col_key:inp_col_key_vals, inp_col_val:inp_col_val_vals})
+
+def test_two_columns_to_dictionary_raises_error_on_duplicate_keys_by_default(duplicate_keys_df: DataFrame):
+    with pytest.raises(ValueError):
+        two_columns_to_dictionary(df= duplicate_keys_df, key_col_name="inp_col_key", value_col_name="inp_col_val", allow_duplicates_keys=False)
+
+def test_two_columns_to_dictionary_allows_duplicate_keys(duplicate_keys_df: DataFrame):
+    exp = {"duplicate":1, "not duplicate":2}
+    res= two_columns_to_dictionary(df= duplicate_keys_df, key_col_name="inp_col_key", value_col_name="inp_col_val", allow_duplicates_keys=True)
+    assert res == exp
