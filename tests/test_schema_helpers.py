d9c088e93b0b65939770a50a952665621cf9bd04
Tom <tomburdge3@gmail.com>
2024-07-01 20:58:15 +0100

    feat: add one and three column tests

--- tests/test_schema_helpers.py
+++ tests/test_schema_helpers.py
@@ -5,12 +5,20 @@ import pytest
 from io import StringIO
 import sys
 
-@pytest.mark.parametrize("prepend_pl, exp", [(True, 'OrderedDict([("col_1", pl.String),("col_2", pl.Array),])'),(False,'OrderedDict([("col_1", String),("col_2", Array),])')])
-def test_print_schema_as_code(prepend_pl:bool, exp:str):
+
+@pytest.mark.parametrize(
+    "schema, prepend_pl, exp",
+    [
+        (OrderedDict([("col_1", T.String), ("col_2", T.Array)]), True, 'OrderedDict([("col_1", pl.String),("col_2", pl.Array),])'),
+        (OrderedDict([("col_1", T.String), ("col_2", T.Array)]), False, 'OrderedDict([("col_1", String),("col_2", Array),])'),
+        (OrderedDict([("col_1", T.String)]), False, 'OrderedDict([("col_1", String),])'),
+        (OrderedDict([("col_1", T.String), ("col_2", T.Array), ("col_3", T.Int64)]), False, 'OrderedDict([("col_1", String),("col_2", Array),("col_3", Int64),])'),
+    ],
+)
+def test_print_schema_as_code(schema: OrderedDict, prepend_pl: bool, exp: str):
     captured_output = StringIO()
     sys.stdout = captured_output
-    schema = OrderedDict([("col_1", T.String), ("col_2", T.Array)])
     print_schema_as_code(schema, prepend_pl)
     sys.stdout = sys.__stdout__
     res = captured_output.getvalue().rstrip("\n")
-    assert res ==exp
+    assert res == exp
