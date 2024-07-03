e1fd5868e076346d5d47c6e0a6ef0e0bcabb8278
Tom Burdge <114760229+TomBurdge@users.noreply.github.com>
2024-07-03 21:16:50 +0100

    Update tests/test_column_functions.py
    Co-authored-by: Lyndon Fan <lyndon0808@gmail.com>

--- tests/test_column_functions.py
+++ tests/test_column_functions.py
@@ -6,8 +6,8 @@ from polars.testing import assert_frame_equal
 
 @pytest.mark.parametrize("frame_type", polars_frames)
 def test_multi_equals(frame_type:DataType):
-    data = frame_type({"col_1": ["cat","dog","pig"],"col_2":["cat","dog","pig"]})
-    exp = DataFrame({"res":[True, False, False]})
+    data = frame_type([{"col_1": "cat", "col_2": "cat"}, {"col_1": "cat", "col_2": "dog"}, {"col_1": "dog", "col_2": "cat"}, {"col_1": "dog", "col_2": "dog"}])
+    exp = DataFrame({"res": [True, False, False, False]})
     if isinstance(res := data.select(res = multi_equals(cols=["col_1", "col_2"],val="cat")), LazyFrame):
         res = res.collect()
     assert_frame_equal(res, exp)
