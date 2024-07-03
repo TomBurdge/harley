5988e5e15bcb0c196a2f0cb0c088394d114a1f24
Tom <tomburdge3@gmail.com>
2024-07-03 21:43:01 +0100

    chore: ruff format null between

--- harley/column_functions.py
+++ harley/column_functions.py
@@ -2,16 +2,17 @@ from polars import Expr
 import polars as pl
 from typing import Any, List
 
+
 def null_between(col: str, lower: str, upper: str) -> Expr:
     return (
         pl.when(pl.col(lower).is_null() & pl.col(upper).is_null())
         .then(False)
         .when(pl.col(col).is_null())
         .then(False)
-.when(pl.col(lower).is_null())
-.then(pl.col(col) <= pl.col(upper))
-.when(pl.col(upper).is_null())
-.then(pl.col(col) >= pl.col(lower))
+        .when(pl.col(lower).is_null())
+        .then(pl.col(col) <= pl.col(upper))
+        .when(pl.col(upper).is_null())
+        .then(pl.col(col) >= pl.col(lower))
         .otherwise(pl.col(col).is_between(pl.col(lower), pl.col(upper)))
     )
 
